dad8aa66307e499e9778e479c771c8decd41fc43
lltyk <lltyk@lltyk>
2012-05-01 10:30:33 -0400

    use contributeFactoryDefaults instead of contributeApplicationDefaults

--- src/main/java/com/github/lltyk/wro4j/services/WRO4JModule.java
+++ src/main/java/com/github/lltyk/wro4j/services/WRO4JModule.java
@@ -1,26 +1,71 @@
 
 package com.github.lltyk.wro4j.services;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.OrderedConfiguration;
+import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.ioc.annotations.InjectService;
 import org.apache.tapestry5.ioc.annotations.Primary;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+import org.apache.tapestry5.services.ApplicationGlobals;
+import org.apache.tapestry5.services.HttpServletRequestFilter;
+import org.apache.tapestry5.services.HttpServletRequestHandler;
 import org.apache.tapestry5.services.assets.ContentTypeAnalyzer;
 import org.apache.tapestry5.services.assets.ResourceMinimizer;
 import org.apache.tapestry5.services.assets.ResourceTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ro.isdc.wro.config.Context;
 import ro.isdc.wro.config.jmx.WroConfiguration;
+import ro.isdc.wro.extensions.model.factory.GroovyModelFactory;
+import ro.isdc.wro.extensions.model.factory.JsonModelFactory;
 import ro.isdc.wro.manager.WroManager;
 import ro.isdc.wro.manager.factory.BaseWroManagerFactory;
+import ro.isdc.wro.model.factory.XmlModelFactory;
 
 
 public class WRO4JModule
 {
-  public static void contributeStreamableResourceSource(MappedConfiguration<String, ResourceTransformer> configuration)
+  private static final Logger log = LoggerFactory.getLogger(WRO4JModule.class);
+
+  public static void contributeFactoryDefaults(MappedConfiguration<String, String> configuration, ApplicationGlobals globals)
   {
-    configuration.addInstance("coffee", CoffeeScriptJsTransformer.class);
-    configuration.addInstance("less", LessCssTransformer.class);
-    configuration.addInstance("sass", SassCssTransformer.class);
+    boolean hasWroFile = hasWroFile(globals);
+    if (hasWroFile) {
+      configuration.add(WRO4JSymbolConstants.ENABLE_WRO_FILTER, "true");
+    } else {
+      configuration.add(WRO4JSymbolConstants.ENABLE_WRO_FILTER, "false");
+    }
+    configuration.add(WRO4JSymbolConstants.ENABLE_LESS_TRANSFORMER, "true");
+    configuration.add(WRO4JSymbolConstants.ENABLE_COFFEESCRIPT_TRANSFORMER, "true");
+    configuration.add(WRO4JSymbolConstants.ENABLE_SASS_TRANSFORMER, "true");
+  }
+
+  public static void bind(ServiceBinder binder)
+  {
+    binder.bind(HttpServletRequestFilter.class, WroFilterWrapper.class).withId("WroFilterWrapper");
+  }
+
+  public static void contributeStreamableResourceSource(MappedConfiguration<String, ResourceTransformer> configuration,
+    @Symbol(WRO4JSymbolConstants.ENABLE_LESS_TRANSFORMER) Boolean enableLess,
+    @Symbol(WRO4JSymbolConstants.ENABLE_COFFEESCRIPT_TRANSFORMER) Boolean enableCoffee,
+    @Symbol(WRO4JSymbolConstants.ENABLE_SASS_TRANSFORMER) Boolean enableSass)
+  {
+    if (enableLess != null && enableLess) {
+      configuration.addInstance("less", LessCssTransformer.class);
+    }
+    if (enableCoffee != null && enableCoffee) {
+      configuration.addInstance("coffee", CoffeeScriptJsTransformer.class);
+    }
+    if (enableSass != null && enableSass) {
+      configuration.addInstance("sass", SassCssTransformer.class);
+    }
   }
 
   @Contribute(ContentTypeAnalyzer.class)
@@ -38,16 +83,81 @@ public class WRO4JModule
     configuration.addInstance("text/css", YuiCssMinimizer.class);
   }
 
-  public WroConfiguration buildDefaultConfiguration()
+  public static WroConfiguration buildDefaultConfiguration()
   {
     return new WroConfiguration();
   }
 
-  public WroManager buildDefaultWroManager()
+  public static WroManager buildDefaultWroManager()
   {
     try {
       Context.set(Context.standaloneContext());
       return new BaseWroManagerFactory().create();
+    } finally {
+      Context.unset();
+    }
+  }
+
+  public static void contributeIgnoredPathsFilter(Configuration<String> configuration)
+  {
+    configuration.add("/wro/");
+  }
+
+  @Contribute(HttpServletRequestHandler.class)
+  public static void httpServletRequestHandler(OrderedConfiguration<HttpServletRequestFilter> configuration,
+    @Symbol(WRO4JSymbolConstants.ENABLE_WRO_FILTER) Boolean enableFilter,
+    @InjectService("WroFilterWrapper") HttpServletRequestFilter wroFilter)
+  {
+    if (enableFilter != null && enableFilter) {
+      log.info("enabling wro filter");
+      configuration.add("WroFilter", wroFilter, "before:IgnoredPaths", "before:GZIP");
+    }
+  }
+
+  private static boolean hasWroFile(ApplicationGlobals globals)
+  {
+    // check for existence of a wro configuration by attempting to create a model
+    try {
+      Context.set(Context.standaloneContext());
+      // getting around Context api...
+      Field f = Context.class.getDeclaredField("servletContext");
+      f.setAccessible(true);
+      f.set(Context.get(), globals.getServletContext());
+      try {
+        return new XmlModelFactory() {
+          public boolean hasWroFile() throws IOException {
+            getModelResourceAsStream();
+            return true;
+          }
+        }.hasWroFile();
+      } catch (Exception e) {
+        log.trace("no wro file", e);
+      }
+      try {
+        return new JsonModelFactory() {
+          public boolean hasWroFile() throws IOException {
+            getModelResourceAsStream();
+            return true;
+          }
+        }.hasWroFile();
+      } catch (Exception e) {
+        log.trace("no wro file", e);
+      }
+      try {
+        return new GroovyModelFactory() {
+          public boolean hasWroFile() throws IOException {
+            getModelResourceAsStream();
+            return true;
+          }
+        }.hasWroFile();
+      } catch (Exception e) {
+        log.trace("no wro file", e);
+      }
+      log.info("No wro file found, automatic wro filter add disabled");
+      return false;
+    } catch (Exception e) {
+      log.error("unexpected error on hasWroFile", e);
+      return false;
     } finally {
       Context.unset();
     }
